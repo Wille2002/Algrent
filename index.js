9c5c58a54ebccf55d911f1b79f3e42c978cf6e2e
Callw02 <muficcalle@gmail.com>
2025-09-08 13:45:53 +0200

    beställ

--- index.js
+++ index.js
@@ -17,7 +17,7 @@ document.getElementById("Form").addEventListener("click", e => {
                     </div>
                     <div class="input">
                         <svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" fill="none"><path d="M20 1V4H23V6H20V9H18V6H15V4H18V1H20ZM12 13C10.9 13 10 12.1 10 11C10 9.9 10.9 9 12 9C13.1 9 14 9.9 14 11C14 12.1 13.1 13 12 13ZM13 3.06V5.08C12.6692 5.02761 12.3349 5.00087 12 5C8.65 5 6 7.57 6 11.2C6 13.54 7.95 16.64 12 20.34C16.05 16.64 18 13.55 18 11.2V11H20V11.2C20 14.52 17.33 18.45 12 23C6.67 18.45 4 14.52 4 11.2C4 6.22 7.8 3 12 3C12.34 3 12.67 3.02 13 3.06Z" fill="black"/></svg>
-                        <input id="organisationsnummer" type="text" placeholder="Organisationsnummer">
+                        <input id="orgNr" type="text" placeholder="Organisationsnummer">
                     </div>
                     <div class="input">
                         <svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" fill="none"><path d="M12 10.9C11.39 10.9 10.9 11.39 10.9 12C10.9 12.61 11.39 13.1 12 13.1C12.61 13.1 13.1 12.61 13.1 12C13.1 11.39 12.61 10.9 12 10.9ZM12 2C6.48 2 2 6.48 2 12C2 17.52 6.48 22 12 22C17.52 22 22 17.52 22 12C22 6.48 17.52 2 12 2ZM14.19 14.19L6 18L9.81 9.81L18 6L14.19 14.19Z" fill="black"/></svg>
@@ -33,7 +33,7 @@ document.getElementById("Form").addEventListener("click", e => {
 
                     <div class="input">
                         <svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" fill="none"><path d="M7 18C5.9 18 5.01 18.9 5.01 20C5.01 21.1 5.9 22 7 22C8.1 22 9 21.1 9 20C9 18.9 8.1 18 7 18ZM1 2V4H3L6.6 11.59L5.25 14.04C5.09 14.32 5 14.65 5 15C5 16.1 5.9 17 7 17H19V15H7.42C7.28 15 7.17 14.89 7.17 14.75L7.2 14.63L8.1 13H15.55C16.3 13 16.96 12.59 17.3 11.97L20.88 5.48C20.9625 5.32739 21.004 5.15598 21.0005 4.98253C20.9969 4.80908 20.9485 4.63951 20.8598 4.49039C20.7711 4.34127 20.6453 4.2177 20.4946 4.13175C20.3439 4.04579 20.1735 4.0004 20 4H5.21L4.27 2H1ZM17 18C15.9 18 15.01 18.9 15.01 20C15.01 21.1 15.9 22 17 22C18.1 22 19 21.1 19 20C19 18.9 18.1 18 17 18Z" fill="black"/></svg>
-                        <input id="amount" type="number" min="0"placeholder="Antal dunkar (5L)">
+                        <input id="antal" type="number" min="0"placeholder="Antal dunkar (5L)">
                     </div>
 
                     <div id="beställKnapp">
@@ -44,17 +44,58 @@ document.getElementById("Form").addEventListener("click", e => {
     `;
 
     document.getElementById("beställKnapp").addEventListener("click", e => {
-       document.querySelector("main").innerHTML = `
-       <div class="confirmOrder">
-            <p id="confirm" >Stämmer denna informationen</p>
-            <p>Bolagsnamn</p>
-            <p>Organisationsnummer</p>
-            <p>Address</p>
-            <p>Leveransaddress</p>
-            
-       </div>`
+       
+        e.preventDefault();
+      
+        let bolagsnamn = document.getElementById("bolagsnamn").value;
+        let orgNr = document.getElementById("orgNr").value;
+        let address = document.getElementById("address").value;
+        let leveransaddress = document.getElementById("leveransaddress").value;
+        let antal = document.getElementById("antal").value;
+      
+       
+        document.querySelector("main").innerHTML = `
+          <div class="container">
+            <h1>Bekräfta Order</h1>
+            <p>Är informationen korrekt?</p>
+      
+            <div class="order-card">
+              <h3>Bolagsnamn</h3>
+              <p>${bolagsnamn}</p>
+      
+              <h3>Organisationsnummer</h3>
+              <p>${orgNr}</p>
+      
+              <h3>Address</h3>
+              <p>${address}</p>
+      
+              <h3>Leveransadress</h3>
+              <p>${leveransaddress}</p>
+      
+              <h3>Antal dunkar (5L)</h3>
+              <p>${antal}</p>
+            </div>
+      
+            <div class="button-group">
+              <button class="back" id="tillbakaKnapp">Tillbaka</button>
+              <button class="confirm" id="bekräftaKnapp">Bekräfta</button>
+            </div>
+          </div>
+        `;
+      
+       
+        document.getElementById("tillbakaKnapp").addEventListener("click", () => {
+          location.reload(); 
+        });
+      
+        document.getElementById("bekräftaKnapp").addEventListener("click", () => {
+          alert("Tack! Din order är bekräftad ✅");
+          
+        });
+      });
+      
         
     })
     
-})
+
 
